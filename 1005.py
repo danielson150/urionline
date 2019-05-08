066f226d3d855f1a3d01efa626428e630997b663
Danielson <danielson150@gmail.com>
2019-05-08 17:46:06 -0300

    atualizacao em 1005.py

--- 1005.py
+++ 1005.py
@@ -1,8 +1,7 @@
-Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 16:07:46) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
-= RESTART: C:/Users/Aluno/AppData/Local/Programs/Python/Python36-32/teste.py =
-0.0
-7.1
-MEDIA = 4.84091
->>> 
+
+A = float (input())
+B = float (input())
+
+M = ((A * 3.5) + (B * 7.5) )/ (3.5+7.5)
+
+print("MEDIA = {:.5f}". format(M))
